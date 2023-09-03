06982a85e95f7c55a687977a2625968b2c45e498
juliakiczka <juliakiczka838@gmail.com>
2023-09-03 12:01:17 +0200

    adding exercise6

--- src/test/java/Exercises.java
+++ src/test/java/Exercises.java
@@ -2,20 +2,26 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class Zadanie4 {
+import java.time.Duration;
+
+public class Exercises {
     private static WebDriver driver;
 
     @BeforeAll
     public static void setUp() {
         driver = new EdgeDriver();
         driver.manage().window().maximize();
+//        use when page is loading for too much time!
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    //  testy automatyczne muszą być niezależne od siebie!
     @Test
-    public void testZadanie4a() {
+    public void testExercise4a() {
         driver.navigate().to("https://sdacademy.dev");
         String pageTitle = driver.getTitle();
         System.out.println(pageTitle);
@@ -23,14 +29,24 @@ public class Zadanie4 {
     }
 
     @Test
-    public void testZadanie4b() {
+    public void testExercise4b() {
         driver.navigate().to("https://stackoverflow.com");
         System.out.println(driver.getTitle());
         Assertions.assertEquals("Stack Overflow - Where Developers Learn, Share, & Build Careers", driver.getTitle());
     }
 
+    @Test
+    public void testExercise6() {
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+        String dashboard = driver.findElement(By.className("oxd-topbar-header-breadcrumb")).getText();
+        Assertions.assertEquals("Dashboard", dashboard);
+    }
+
     @AfterAll
     public static void tearDown() {
-        driver.quit();
+//        driver.quit();
     }
 }
